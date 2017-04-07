bc5f23c8af37883cf9487c29fcbbe9ce51f8312f
Ivan Kravets <me@ikravets.com>
2017-04-07 19:48:09 +0300

    Handle ARM 64bits architecture

--- platform.py
+++ platform.py
@@ -18,17 +18,20 @@ from platformio.managers.platform import PlatformBase
 
 class Linux_armPlatform(PlatformBase):
 
+    @staticmethod
+    def _is_native():
+        systype = util.get_systype()
+        return "linux_arm" in systype or "linux_aarch64" in systype
+
     @property
     def packages(self):
         packages = PlatformBase.packages.fget(self)
-        if ("linux_arm" in util.get_systype() and
-                "toolchain-gccarmlinuxgnueabi" in packages):
+        if self._is_native() and "toolchain-gccarmlinuxgnueabi" in packages:
             del packages['toolchain-gccarmlinuxgnueabi']
         return packages
 
     def configure_default_packages(self, variables, targets):
-        if ("wiringpi" in variables.get("pioframework") and
-                "linux_arm" not in util.get_systype()):
+        if self._is_native() and "wiringpi" in variables.get("pioframework"):
             raise exception.PlatformioException(
                 "PlatformIO temporary does not support cross-compilation "
                 "for WiringPi framework. Please run PlatformIO directly on "
